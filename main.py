402afbbcaefc4c794c3e0c01a3ef715b5edfd55d
maxwell-adam <maxwell.adam@me.com>
2015-07-12 18:52:33 +1000

    Adding more resources

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def hello():
     	<html>
     	<a href="/account/maxwell.adam">account</a>
     	<a href="/business/goodyear_tires">business</a>
+    	<a href="/bankaccount/maxwell.adam">bankaccount</a>
     	</html>
     	"""
 
@@ -21,7 +22,7 @@ def account(username):
     """Return the matching account resource"""
     account = {}
     account['name'] = username
-    account['gold'] = 100
+    #account['gold'] = 100
     return str(account)
 
 @app.route('/business/<businessname>')
@@ -36,14 +37,22 @@ def business(businessname):
     
 @app.route('/product/<productname>')
 def Physical_products(productname):
-    """Return the matching business resource"""
+    """Return the matching product resource"""
     Physical_products = {}
     Physical_products['name'] = productname
     Physical_products['price'] = 0.5
     return str(Physical_products)
     
-    
+@app.route('/bankaccount/<username>')
+def bankaccount(username):
+    """Return the matching business resource"""
+    bankaccount= {}
+    bankaccount['name'] = username + "\'s bank account"
+    bankaccount['owner'] = username
+    bankaccount['balance'] = 1
+    return str(bankaccount)
+
 @app.errorhandler(404)
 def page_not_found(e):
     """Return a custom 404 error."""
-    return 'Sorry, nothing at this URL.', 404
+    return 'Sorry, you stuffed this URL.', 404
